667f9891fe1fe755ef31e5e1855a1a9cf01ee3ed
cep104 <candicelizabethpeters@gmail.com>
2020-11-09 17:50:20 -0600

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,33 +3,38 @@ array.sort
 end
 
 def sort_array_desc(array)
-  (array.sort).reverse
+  # (array.sort).reverse
+  array.sort do |a,b|
+  b <=> a
+  end
 end
 
 def sort_array_char_count(array)
-  array.sort do |a, b|
+  array.sort do |a, b| #sort by character count of each word
   a.length <=> b.length
 end
 end
 
 def swap_elements(array)
-
+#swap the second and third elements
  array[1], array[2] = array[2], array[1]
 array
 end
 
 def reverse_array(array)
-  
    array.reverse
  
 end
 
 def kesha_maker(array)
   new_array = []
-  array.each do|string|
- new_array <<  string[2] = "$"
-  new_array
+#   array.each do|string|
+#  new_array <<  string[2] = "$"
+#   new_array
+array.map do |string|
+  string[2] = "$"
   end
+  array
 end
 
 def find_a(array)
@@ -38,11 +43,13 @@ end
 
 def sum_array(array)
   array.inject { |sum, n| sum + n }
+  #https://apidock.com/ruby/Enumerable/inject
 end
 
 def add_s(array)
  
   array.each_with_index do |element, index|
     index != 1 ? element << "s" : element
+    #if the index doesnt equal array[1] add an s to end of element
 end
 end
